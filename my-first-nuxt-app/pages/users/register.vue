41b192e8add72505257951187304fd4794ee2acb
juchilian <juchilian@outlook.jp>
2019-12-07 14:10:57 +0900

    [add] my-first-nuxt-appフォルダ
    本第一章

---
+++ my-first-nuxt-app/pages/users/register.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container">
+        <p>
+
+            /users/register.vue
+        </p>
+        <p class="red">goodbye</p>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+.container {
+    width: 80vw;
+    p {
+        font-size: 20px;
+    }
+    .red {
+        color: red;
+    }
+}
+</style>
