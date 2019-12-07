1a423cf82454aca2ac05464eb69ec4ccef0d8a85
juchilian <juchilian@outlook.jp>
2019-12-07 17:47:43 +0900

    [finish] chapter3 section 22 plugin

---
+++ chapter3/pages/authed-route.vue
@@ -0,0 +1,7 @@
+<template lang="pug">
+  div
+    h2 認証が必要なページ
+    p Cookie の 'credential' 値がTruthyな場合のみアクセス可能です。
+    br
+    nuxt-link(to="/") トップページへ戻る
+</template>
---
+++ chapter3/pages/login.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+  div
+    h2 ログインページへ
+    p
+      button(type="button")(@click="login") ログイン
+      br
+      nuxt-link(to="/") トップページへ戻る
+</template>
+
+<script>
+import Cookies from 'universal-cookie'
+export default {
+  methods: {
+    login() {
+      const cookies = new Cookies()
+      cookies.set('credential', true, {maxAge:90})
+      this.$router.push('/')
+    }
+  }
+}
+</script>
